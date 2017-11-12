695ce1fcd5cc86256325744cd0a332de1a76a08c
vijayadevan <vijayadevantheboss@gmail.com>
2017-11-12 13:38:27 +0530

    Added Update operation

--- src/user.js
+++ src/user.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    name: String
+    name: String,
+    postCount: Number
 });
 
 var User = mongoose.model('user', userSchema);
